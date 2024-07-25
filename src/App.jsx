08fa0102416c39ee5ec40e44f1e398f7f5cd0fc0
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-07-25 10:57:32 -0400

    Added handleClick for adding, subtracting and multiplying

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
 import './App.css'
 
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClickCount = () => {
+    setCount(count + 5);
+  }
+
+  const handleClickDecreaseCount = () => {
+    if (count > 1) {
+      setCount(count - 2);
+    }
+  }
+
+  const handleMultiplyClick = () => {
+    setCount(count * 5);
+  }
 
   return (
-    <div className='App'></div>
+    <div className='App'>
+      <div>Counter Value = {count} </div>
+      <button onClick={handleClickCount}> +5 </button>
+      <button onClick={handleClickDecreaseCount}> -2</button>
+      <button onClick={handleMultiplyClick}>* 5 </button>
+
+    </div>
   )
 }
 
